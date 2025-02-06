3e27f58bc7a0a7f50d592ea742f06ac4918198ed
pharaoh2012 <2225220+pharaoh2012@users.noreply.github.com>
2025-02-06 13:42:45 +0800

    Update deloldfile.sh

--- deloldfile.sh
+++ deloldfile.sh
@@ -4,7 +4,7 @@
 cd out/cache
 
 # 列出所有文件按修改时间排序，只保留最新的1000个文件
-find . -type f -printf '%T@ %p\n' | sort -n | head -n -1000 | cut -d' ' -f2- | xargs -r rm
+find . -type f -printf '%T@ %p\n' | sort -n | head -n -3000 | cut -d' ' -f2- | xargs -r rm
 
 # 显示删除后的文件数量
 echo "当前缓存文件数量：$(find . -type f | wc -l)"
