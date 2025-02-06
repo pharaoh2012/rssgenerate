953fef7a16c89e58dc7a0a811bc71e8c8fe9d201
pharaoh2012 <2225220+pharaoh2012@users.noreply.github.com>
2025-02-06 10:53:49 +0800

    忽略脚本错误

--- main.sh
+++ main.sh
@@ -1,8 +1,8 @@
 
-node ./rss/0818tuan.js
-# node ./rss/51credit.js
-node ./rss/52pojie.js
-node ./rss/cnblogs.js
-node ./rss/juejin.js
-node ./rss/smzdm.js
-node ./rss/52pojie_aiqiyi.js
+node ./rss/0818tuan.js || true
+# node ./rss/51credit.js || true
+node ./rss/52pojie.js || true
+node ./rss/cnblogs.js || true
+node ./rss/juejin.js || true
+node ./rss/smzdm.js || true
+node ./rss/52pojie_aiqiyi.js || true
